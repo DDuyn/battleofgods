539560470db20d5183b7e7a68d6f5f708195ca31
David Duyn <duyndavid@gmail.com>
2020-09-26 21:20:43 +0200

    Add Connection"

--- app/src/Infrastructure/Database/database.module.ts
+++ app/src/Infrastructure/Database/database.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
@@ -9,7 +7,5 @@ import { MongooseModule } from '@nestjs/mongoose';
       'mongodb+srv://admin20:admin@cluster0.cj6ks.mongodb.net/BATTLEOFGODS?retryWrites=true&w=majority',
     ),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {}
+export class DatabaseModule {}
